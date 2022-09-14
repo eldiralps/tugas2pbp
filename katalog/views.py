fcb36ee535387297e57bb1cc20488dba6f3ca489
eldiralps <eldiralps@gmail.com>
2022-09-14 09:53:57 +0700

    done MVT

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from wishlist.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    return render(request, "katalog.html")
+    
